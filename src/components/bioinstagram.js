6ac5567b38fce8609152e7422cc6791be73eaa1b
xoyoc <xoyoc_l2@hotmail.com>
2022-12-25 23:33:20 -0600

    Cambios en biografia

--- src/components/bioinstagram.js
+++ src/components/bioinstagram.js
@@ -2,12 +2,10 @@ import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import { Card, CardActions, CardHeader, Typography } from "@mui/material";
+import { Card, CardActions, CardHeader } from "@mui/material";
 
 
 const BioInstagram = () => {
-  const author = "Antonio Xoyoc Becerra Farias"
-
   return (
     <div className="bio">
       <StaticImage
@@ -15,29 +13,35 @@ const BioInstagram = () => {
         layout="fixed"
         formats={["auto", "webp", "avif"]}
         src="../images/yo.jpg"
-        width={100}
+        width={120}
         height={100}
         quality={95}
         alt="Foto de perfil"
       />
-      {author?.name && (
-        <Container>
-          <Box>
-            <Card>
-              <CardHeader>
-                <Typography>Mi nombre es <strong>{author.name}</strong></Typography>
-              </CardHeader>
-              <p>Teléfono: 7531043367</p>
-              <p>Email: <a href="mailto:xoyoc_l2@hotmail.com">xoyoc_l2@hotmail.com</a></p>
-              <CardActions>
-                <a href="https://www.xoyoc.net">
-                  Mi sitio Web
-                </a>
-              </CardActions>
-            </Card>
-          </Box>
-        </Container>
-      )}
+      <Container>
+        <Box>
+          <Card>
+            <CardHeader
+              title="Antonio Xoyoc Becerra Farias"
+              actAsExpander={true}
+              showExpandableButton={true}
+            />
+            <p>Teléfono: 7531043367</p>
+            <p>Email: <a href="mailto:xoyoc_l2@hotmail.com">xoyoc_l2@hotmail.com</a></p>
+            <CardActions>
+              <a href="https://www.xoyoc.net">
+                Mi sitio Web
+              </a>
+              <a href="https://www.xoyoc.net">
+                Youtube
+              </a>
+              <a href="https://www.xoyoc.net">
+                Instagram
+              </a>
+            </CardActions>
+          </Card>
+        </Box>
+      </Container>
     </div>
   )
 }
